637ad932cf266777f4fc507d4581bb84e6e9ae4b
RAD 김주형 <kim_joohyoung@radcns.com>
2024-03-27 13:20:28 +0900

    프리티 적용 및 리엑트 종속성 추가

--- view/components/option/Option.ts
+++ view/components/option/Option.ts
@@ -1,32 +1,29 @@
 import { from, of, Observable } from 'rxjs';
-import styles from './Button.module.css'
+import styles from './Button.module.css';
 
 export type OptionAttribute = {
-    textContent?: string
-    value?: string | number
-    id?: string
-	event?: Partial<GlobalEventHandlers>
-}
+    textContent?: string;
+    value?: string | number;
+    id?: string;
+    event?: Partial<GlobalEventHandlers>;
+};
 export type OptionStyle = {
-	size?: 'initial' | 'inherit' | 'long' | 'short' | 'middle'
-	animation?: 'spin'
+    size?: 'initial' | 'inherit' | 'long' | 'short' | 'middle';
+    animation?: 'spin';
 };
 
-export const option = ( (
-	{textContent, value, event = {}} : OptionAttribute,
-	{size = 'inherit',} : OptionStyle = {},
-) => {
-	let promise = new Promise<HTMLOptionElement>(res=>{
-		let option = Object.assign(document.createElement('option'), {
-			className:`${styles.button} ${styles[size]}`,
+export const option = ({ textContent, value, event = {} }: OptionAttribute, { size = 'inherit' }: OptionStyle = {}) => {
+    let promise = new Promise<HTMLOptionElement>((res) => {
+        let option = Object.assign(document.createElement('option'), {
+            className: `${styles.button} ${styles[size]}`,
             textContent,
-            value
-		});
-		Object.assign(option, event);
-		/*if(animation) {
+            value,
+        });
+        Object.assign(option, event);
+        /*if(animation) {
 			button.classList.add(styles[animation]);
 		}*/
-		res(option);
-	})
-	return from(promise)
-})
+        res(option);
+    });
+    return from(promise);
+};
--- view/components/select/Select.ts
+++ view/components/select/Select.ts
@@ -1,55 +1,43 @@
 import { from, of, Observable, mergeMap, map, zip, toArray } from 'rxjs';
-import styles from './Button.module.css'
+import styles from './Button.module.css';
 import { option } from '@components/option/Option';
 
 // components -> container -> wrapper
 
 export type SelectAttribute = {
-	event?: Partial<GlobalEventHandlers>
-}
+    event?: Partial<GlobalEventHandlers>;
+};
 export type SelectStyle = {
-	size?: 'initial' | 'inherit' | 'long' | 'short' | 'middle'
-	animation?: 'spin'
-	svg?:string,
-	svgPosition?: 'top' | 'bottom' | 'left' | 'right'
+    size?: 'initial' | 'inherit' | 'long' | 'short' | 'middle';
+    animation?: 'spin';
+    svg?: string;
+    svgPosition?: 'top' | 'bottom' | 'left' | 'right';
 };
 
+export const select = ({ event = {} }: SelectAttribute, { size = 'inherit', animation, svg }: SelectStyle = {}, options: Observable<HTMLOptionElement[]>) => {
+    let promise = new Promise<HTMLSelectElement>((res) => {
+        let select = Object.assign(document.createElement('select'), {
+            className: `${styles.button} ${styles[size]} ${svg && styles.svg}`,
+        });
+        Object.assign(select, event);
 
-export const select = ( (
-	{event = {}} : SelectAttribute,
-	{size = 'inherit', animation, svg} : SelectStyle = {},
-	options: Observable<HTMLOptionElement[]>,
-) => {
-	let promise = new Promise<HTMLSelectElement>(res=>{
-		let select = Object.assign(document.createElement('select'), {
-			className:`${styles.button} ${styles[size]} ${svg && styles.svg}`,
-		});
-		Object.assign(select, event);
-		
-		/*if(animation) {
+        /*if(animation) {
 			button.classList.add(styles[animation]);
 		}*/
-		
-		options.subscribe(options=>{
-			select.replaceChildren(...options);
-		})
-		res(select);
-	})
-	return from(promise)
-})
-let t = zip(option({},{}), option({},{})).pipe(
-	mergeMap(e=>e),
-	toArray(),
-	map(e=>{
-		console.log(e);
-	})
-)
-console.log(t);
-select(
-	{},
-	{},
-	zip(
-		option({},{})
-	)
-);
 
+        options.subscribe((options) => {
+            select.replaceChildren(...options);
+        });
+        res(select);
+    });
+    return from(promise);
+};
+let t = zip(option({}, {}), option({}, {})).pipe(
+    mergeMap((e) => e),
+    toArray(),
+    map((e) => {
+        console.log(e);
+    }),
+);
+console.log(t);
+select({}, {}, zip(option({}, {})));
