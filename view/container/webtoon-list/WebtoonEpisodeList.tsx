2ec83c1bfaea8e8adc37c6b11a2f5932ded5d040
joohyoungkim19940805 <oozu1994@gmail.com>
2024-04-11 23:03:48 +0900

    웹툰목록디자인개선

--- view/container/webtoon-list/WebtoonEpisodeList.tsx
+++ view/container/webtoon-list/WebtoonEpisodeList.tsx
@@ -1,5 +1,5 @@
 import { WebtoonEpisodeType } from '@type/WebtoonEpisodeType';
-
+import styles from './WebtoonEpisodeList.module.css';
 const testWebtoonList = [
     [...new Array(20)],
     [...new Array(20)],
@@ -32,23 +32,39 @@ export const WebtoonEpisodeList = () => {
             <ul>
                 {testWebtoonList[0].map((item) => {
                     return (
-                        <li>
-                            <div>
-                                <div>
-                                    <img src="/"></img>
+                        <li
+                            className={`${styles['webtoon-episode-container']}`}
+                        >
+                            <div className={`${styles['episode-thumbnail']}`}>
+                                <img src="https://manatoki333.net/data/file/comic/526342/19200461/thumb-mlZHrEwjMXS0_240x320.jpg"></img>
+                            </div>
+                            <div className={`${styles['episode-title']}`}>
+                                <div className={`${styles['title-wrapper']}`}>
+                                    <div className={`${styles['title']}`}>
+                                        {item.title}
+                                    </div>
+                                    <div className={`${styles['chapter']}`}>
+                                        {`${item.episode.chapter} 화`}
+                                        <span
+                                            className={`${styles['sub-title']}`}
+                                        >
+                                            {item.episode.subTitle}
+                                        </span>
+                                    </div>
                                 </div>
-                                <div>
-                                    <span>{item.title}</span>
-                                    <span>{item.episode.subTitle}</span>
-                                    <span>{`[ ${item.episode.chapter}화 ]`}</span>
+                                <div className={`${styles['episode-info']}`}>
+                                    <div>작가 이름</div>
+                                    <div>장르 종류</div>
                                 </div>
-                                <div>
-                                    <span>
-                                        {new Date(
-                                            item.episode.createAt,
-                                        ).toLocaleString()}
-                                    </span>
-                                    <span>{item.episode.views || 0}</span>
+                            </div>
+                            <div className={`${styles['episode-at']}`}>
+                                <div className={`${styles['at']}`}>
+                                    {new Date(
+                                        item.episode.createAt,
+                                    ).toLocaleString()}
+                                </div>
+                                <div className={`${styles['views']}`}>
+                                    {item.episode.views || 0}
                                 </div>
                             </div>
                         </li>
