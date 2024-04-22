c517c955f282b8033237c0acb1b605a63455b192
joohyoungkim19940805 <oozu1994@gmail.com>
2024-04-22 20:36:06 +0900

    테스트 셋팅 중

--- view/handler/Subject/LayerEvent.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactElement, ReactNode } from 'react';
-import { Subject } from 'rxjs';
-
-export const $globalDimLayer = new Subject<
-    ReactNode | ReactElement | JSX.Element | undefined
->();
