809b5154e7b16bccb889baf6fa804cd1b6b5907f
joohyoungkim19940805 <oozu1994@gmail.com>
2024-04-22 20:44:25 +0900

    줄바꿈수정

--- src/test/java/com/our/all/webtoon/TestAccountHandler.java
+++ src/test/java/com/our/all/webtoon/TestAccountHandler.java
@@ -26,55 +26,61 @@ public class TestAccountHandler {
     private AccountRepository accountRepository;
 
     private WebTestClient webTestClient;
-
-    //
+//
     @Before
     public void setUp() {
         webTestClient = WebTestClient.bindToApplicationContext(context).build();
     }
 
     /*
-     * @Test public void testGetUserById() { User user =
-     * User.builder().id(1).name("ABC").email("abc@xyz.com").build(); Mono<User> UserMono =
-     * Mono.just(user); when(userRepository.findById(1)).thenReturn(UserMono);
-     * webTestClient.get().uri("/users/1").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().
-     * isOk().expectBody(User.class).value(userResponse -> {
-     * Assertions.assertThat(userResponse.getId()).isEqualTo(1);
-     * Assertions.assertThat(userResponse.getName()).isEqualTo("ABC");
-     * Assertions.assertThat(userResponse.getEmail()).isEqualTo("abc@xyz.com"); });
-     * 
-     * }
-     * 
-     * @Test public void testGetUsers() {
-     * 
-     * User user1 = User.builder().id(1).name("ABC").email("abc@xyz.com").build(); User user2 =
-     * User.builder().id(2).name("XYZ").email("xyz@abc.com").build();
-     * 
-     * when(userRepository.findAll()).thenReturn(Flux.just(user1, user2));
-     * 
-     * webTestClient.get().uri("/users").exchange().expectStatus().isOk().expectBodyList(User.class).
-     * value(userResponse -> { Assertions.assertThat(userResponse.get(0).getId()).isEqualTo(1);
-     * Assertions.assertThat(userResponse.get(0).getName()).isEqualTo("ABC");
-     * Assertions.assertThat(userResponse.get(0).getEmail()).isEqualTo("abc@xyz.com");
-     * Assertions.assertThat(userResponse.get(1).getId()).isEqualTo(2);
-     * Assertions.assertThat(userResponse.get(1).getName()).isEqualTo("XYZ");
-     * Assertions.assertThat(userResponse.get(1).getEmail()).isEqualTo("xyz@abc.com");
-     * 
-     * });
-     * 
-     * }
-     * 
-     * @Test public void testCreateUser() {
-     * 
-     * User user = User.builder().id(1).name("ABC").email("abc@xyz.com").build(); Mono<User> UserMono =
-     * Mono.just(user); when(userRepository.save(any())).thenReturn(UserMono);
-     * 
-     * webTestClient
-     * .post().uri("/users").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).
-     * body(Mono.just(user), User.class).exchange().expectStatus().isOk().expectBody(User.class)
-     * .value(userResponse -> { Assertions.assertThat(userResponse.getId()).isEqualTo(1);
-     * Assertions.assertThat(userResponse.getName()).isEqualTo("ABC");
-     * Assertions.assertThat(userResponse.getEmail()).isEqualTo("abc@xyz.com"); }); }
-     */
+    @Test
+    public void testGetUserById() {
+        User user = User.builder().id(1).name("ABC").email("abc@xyz.com").build();
+        Mono<User> UserMono = Mono.just(user);
+        when(userRepository.findById(1)).thenReturn(UserMono);
+        webTestClient.get().uri("/users/1").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(User.class).value(userResponse -> {
+            Assertions.assertThat(userResponse.getId()).isEqualTo(1);
+            Assertions.assertThat(userResponse.getName()).isEqualTo("ABC");
+            Assertions.assertThat(userResponse.getEmail()).isEqualTo("abc@xyz.com");
+        });
 
+    }
+
+    @Test
+    public void testGetUsers() {
+
+        User user1 = User.builder().id(1).name("ABC").email("abc@xyz.com").build();
+        User user2 = User.builder().id(2).name("XYZ").email("xyz@abc.com").build();
+
+        when(userRepository.findAll()).thenReturn(Flux.just(user1, user2));
+
+        webTestClient.get().uri("/users").exchange().expectStatus().isOk().expectBodyList(User.class).value(userResponse -> {
+            Assertions.assertThat(userResponse.get(0).getId()).isEqualTo(1);
+            Assertions.assertThat(userResponse.get(0).getName()).isEqualTo("ABC");
+            Assertions.assertThat(userResponse.get(0).getEmail()).isEqualTo("abc@xyz.com");
+            Assertions.assertThat(userResponse.get(1).getId()).isEqualTo(2);
+            Assertions.assertThat(userResponse.get(1).getName()).isEqualTo("XYZ");
+            Assertions.assertThat(userResponse.get(1).getEmail()).isEqualTo("xyz@abc.com");
+
+        });
+
+    }
+
+    @Test
+    public void testCreateUser() {
+
+        User user = User.builder().id(1).name("ABC").email("abc@xyz.com").build();
+        Mono<User> UserMono = Mono.just(user);
+        when(userRepository.save(any())).thenReturn(UserMono);
+
+        webTestClient
+            .post().uri("/users").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).body(Mono.just(user), User.class).exchange().expectStatus().isOk().expectBody(User.class)
+            .value(userResponse -> {
+                Assertions.assertThat(userResponse.getId()).isEqualTo(1);
+                Assertions.assertThat(userResponse.getName()).isEqualTo("ABC");
+                Assertions.assertThat(userResponse.getEmail()).isEqualTo("abc@xyz.com");
+            });
+    }
+    */
+    
 }
