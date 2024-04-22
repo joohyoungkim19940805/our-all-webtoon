0771e8b893e5c81777d90f7d1de4374ff49ec8b1
joohyoungkim19940805 <oozu1994@gmail.com>
2024-04-22 21:34:38 +0900

    ㅁ

--- view/container/login/FindAccountInfoContainer.tsx
+++ view/container/login/FindAccountInfoContainer.tsx
@@ -4,12 +4,24 @@ import styles from './FindAccountInfoContainer.module.css';
 
 export const FindAccountInfo = () => {
     return (
-        <div className={styles['find-account-info-container']}>
-            <a className={styles['find-account-info-container']}>회원 가입</a>
+        <>
             <div className={styles['find-account-info-container']}>
-                <a>아이디 찾기</a>
-                <a>비밀번호 찾기</a>
+                <a className={styles['find-account-info-container']}>
+                    회원 가입
+                </a>
+                <div className={styles['find-account-info-container']}>
+                    <a>아이디 찾기</a>
+                    <a>비밀번호 찾기</a>
+                </div>
             </div>
-        </div>
+            <div>
+                <ul>
+                    <li>구글</li>
+                    <li>카카오톡</li>
+                    <li>네이버</li>
+                    <li>애플</li>
+                </ul>
+            </div>
+        </>
     );
 };
