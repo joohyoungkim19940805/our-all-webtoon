6ad0a00381f033ebea18304f9eb0f86b0750517f
joohyoungkim19940805 <oozu1994@gmail.com>
2024-04-24 21:39:53 +0900

    a

--- view/container/login/FindAccountInfoContainer.tsx
+++ view/container/login/FindAccountInfoContainer.tsx
@@ -1,17 +1,18 @@
 import { from, map, zip } from 'rxjs';
 import { Anchor } from '@components/anchor/Anchor';
 import styles from './FindAccountInfoContainer.module.css';
+import { Link } from 'react-router-dom';
 
 export const FindAccountInfo = () => {
     return (
         <>
             <div className={styles['find-account-info-container']}>
-                <a className={styles['find-account-info-container']}>
+                <Link to="/" className={styles['find-account-info-container']}>
                     회원 가입
-                </a>
+                </Link>
                 <div className={styles['find-account-info-container']}>
-                    <a>아이디 찾기</a>
-                    <a>비밀번호 찾기</a>
+                    <Link to="/">아이디 찾기</Link>
+                    <Link to="/">비밀번호 찾기</Link>
                 </div>
             </div>
             <div>
