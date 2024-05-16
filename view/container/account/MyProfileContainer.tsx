33a16efd6fa5c2d2fe72b7424b56d39025f7a92a
joohyoungkim19940805 <oozu1994@gmail.com>
2024-05-16 23:03:29 +0900

    aa

--- view/container/account/MyProfileContainer.tsx
+++ view/container/account/MyProfileContainer.tsx
@@ -7,7 +7,7 @@ import { useEffect, useRef, useState } from 'react';
 export const MyProfileContainer = () => {
     const ref = useRef<HTMLFormElement>(null);
     const [account, setAccount] = useState<Account>();
-
+    const [passwordInputLength, setPasswordInputLength] = useState<number>(0);
     useEffect(() => {
         const subscribe = getAccountInfoService().subscribe({
             next: (account) => {
@@ -18,6 +18,7 @@ export const MyProfileContainer = () => {
             subscribe.unsubscribe();
         };
     }, [ref]);
+
     /**
      * {
     "accountName": "김주형",
@@ -46,13 +47,21 @@ export const MyProfileContainer = () => {
      */
     return (
         <div className={`${styles['my-profile-container']}`}>
-            <form className={`${styles['my-profile-form']}`} ref={ref}>
+            <form
+                className={`${styles['my-profile-form']}`}
+                ref={ref}
+                method="POST"
+            >
+                <div className={`${styles['submit-button-wrapper']}`}>
+                    <button type="submit">등록</button>
+                </div>
                 <div>
                     <label htmlFor="account-name">ID</label>
                     <input
                         type="text"
                         id="account-name"
                         name="accountName"
+                        autoComplete="username"
                         className={`${inputStyles.input} ${inputStyles['bright-purple']}`}
                         readOnly
                         value={(account && account.accountName) || ''}
@@ -61,18 +70,37 @@ export const MyProfileContainer = () => {
                 <div>
                     <label htmlFor="account-password">PASSWORD</label>
                     <input
+                        onInput={(ev) =>
+                            setPasswordInputLength(
+                                ev.currentTarget.value.length,
+                            )
+                        }
                         type="password"
                         id="account-password"
                         name="password"
+                        autoComplete="current-password"
                         className={`${inputStyles.input} ${inputStyles['bright-purple']}`}
                     ></input>
                 </div>
+                {passwordInputLength > 0 && (
+                    <div>
+                        <label htmlFor="account-re-password"></label>
+                        <input
+                            type="password"
+                            id="account-re-password"
+                            placeholder="비밀번호 재입력"
+                            name="account-re-password"
+                            autoComplete="new-password"
+                        ></input>
+                    </div>
+                )}
                 <div>
-                    <label htmlFor="account-age">EMAIL</label>
+                    <label htmlFor="account-email">EMAIL</label>
                     <input
                         type="email"
                         id="account-email"
                         name="email"
+                        autoComplete="username"
                         className={`${inputStyles.input} ${inputStyles['bright-purple']}`}
                         defaultValue={(account && account.email) || ''}
                     ></input>
@@ -94,7 +122,7 @@ export const MyProfileContainer = () => {
                         name="gender"
                         className={`${inputStyles.input} ${inputStyles['bright-purple']}`}
                     >
-                        <option>what is your gender?</option>
+                        <option disabled>what is your gender?</option>
                         <option
                             value="MALE"
                             selected={account && account.gender === 'MALE'}
@@ -114,6 +142,16 @@ export const MyProfileContainer = () => {
                             논바이너리
                         </option>
                     </select>
+                </div>
+                <div>
+                    <label>NICKNAME</label>
+                    <input
+                        type="text"
+                        id="account-nickname"
+                        name="nickname"
+                        className={`${inputStyles.input} ${inputStyles['bright-purple']}`}
+                        defaultValue={(account && account.nickname) || ''}
+                    ></input>
                 </div>
             </form>
         </div>
