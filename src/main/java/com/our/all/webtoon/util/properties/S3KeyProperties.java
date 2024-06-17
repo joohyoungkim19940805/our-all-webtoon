b8d1f4612427b043ba41ee9dad1e89164a89f709
joohyoungkim19940805 <oozu1994@gmail.com>
2024-06-17 13:00:43 +0900

    테스트 코드 제거

--- src/main/java/com/our/all/webtoon/util/properties/S3KeyProperties.java
+++ src/main/java/com/our/all/webtoon/util/properties/S3KeyProperties.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import reactor.core.publisher.Mono;
 
 
 @ToString
@@ -17,16 +16,4 @@ import reactor.core.publisher.Mono;
 public class S3KeyProperties {
 
 
-	public static void main(
-		String a[]
-	) {
-
-		Mono<String> mono1 = Mono.just( "A" );
-		Mono<String> mono2 = Mono.just( "B" );
-
-		Mono<String> mergedMono = mono1.zipWith( mono2, (s1, s2) -> s1 + s2 );
-
-		mergedMono.subscribe( System.out::println );
-
-	}
 }
